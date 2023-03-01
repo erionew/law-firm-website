990d3318c8c3b98a3ce7d99a29c7cd783b64faf6
Erione White <erionewhite1999@gmail.com>
2023-02-28 20:00:11 -0600

    finished header component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Header aboutPath='#' testimonialsPath='#' expertisePath='#' contactPath='#' />
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './Header.css'
+import balanceIcon from '../pics/balance.png'
+import lawyers from '../pics/lawyers.jpg'
+
+
 
 export default function Header({ aboutPath, testimonialsPath, expertisePath, contactPath }) {
+
   return (
-    <>
+    <div className='container--header'>
       <div className='container--desktop-menu'>
         <div>
-            <h1>Justice Firm</h1>
-            <button className='button--white'>Free Evaluation</button>
+            <h1>Justice Law Firm<span className='text--gold'>.</span></h1>
+            <button className='button--white'>Free Evaluation<img src={balanceIcon}></img></button>
         </div>
-        <ul className='container-nav-links'>
+        <ul className='container--nav-links'>
             <li><a className='nav-link' href={aboutPath}>About</a></li>
             <li><a className='nav-link' href={testimonialsPath}>Testimonials</a></li>
             <li><a className='nav-link' href={expertisePath}>Our Expertise</a></li>
@@ -18,17 +23,26 @@ export default function Header({ aboutPath, testimonialsPath, expertisePath, con
       </div>
 
       <div className='container--mobile-menu'>
-        <div className='mobile-menu--nav-bar'>
-            <h1>Justice Firm</h1>
+        <div className='mobile--nav-bar'>
+            <h1>Justice Law Firm<span className='text--gold'>.</span></h1>
+            <i className="fa-solid fa-bars" onClick={() => {
+              document.querySelector('.mobile--nav-links').classList.toggle('active')
+            }
+            }></i>
         </div>
-        <ul className='container-nav-links'>
+        <ul className='mobile--nav-links'>
             <li><a className='nav-link' href={aboutPath}>About</a></li>
             <li><a className='nav-link' href={testimonialsPath}>Testimonials</a></li>
             <li><a className='nav-link' href={expertisePath}>Our Expertise</a></li>
             <li><a className='nav-link' href={contactPath}>Contact</a></li>
+            <button className='button--white'>Free Evaluation<img src={balanceIcon}></img></button>
+            
         </ul>
       </div>
-      <img></img>
-    </>
+      <div className='container--header-img'>
+        <h2 className='header-img--caption'>Justice Oriented.</h2>
+        <p className='header-img--sub-caption'>We're here to help you.</p>
+      </div>
+    </div>
   )
 }
